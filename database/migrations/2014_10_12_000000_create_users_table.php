fd2eb07dfd8a41999e56875b8460548397fde0ea
labayanalbert <ajlabayan@thinkbigcreatives.com>
2021-06-16 14:45:03 +0800

    altered some database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,8 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->text('program');
-            $table->integer('student');
+            $table->integer('student')->nullable();
+            $table->integer('employee')->nullable();
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->text('profile_photo_path')->nullable();
