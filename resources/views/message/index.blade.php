c4520faffd768ce9e8cf1aa373032b128b8ef872
labayanalbert <ajlabayan@thinkbigcreatives.com>
2021-10-06 15:03:19 +0800

    commit

--- resources/views/message/index.blade.php
+++ resources/views/message/index.blade.php
@@ -4,9 +4,10 @@
             <!-- headaer -->
             <div class="flex items-center justify-between px-5 py-5 bg-white border-b-2 ">
                 <div class="text-2xl font-semibold">Live Chat</div>
-                
-                <img class="w-10 h-10 rounded-full" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
-                
+
+                <img class="w-10 h-10 rounded-full" src="{{ Auth::user()->profile_photo_url }}"
+                    alt="{{ Auth::user()->name }}" />
+
             </div>
             <!-- end header -->
             <!-- Chatting -->
@@ -14,14 +15,15 @@
                 <!-- message -->
                 <div class="flex flex-col justify-between w-full px-5">
                     <div class="p-6 border-t border-gray-200 md:border-t-0 md:border-l">
-                        <div class="flex flex-col flex-auto w-full h-64 overflow-y-auto antialiased text-gray-800 bg-gray-300 rounded-t-lg">
+                        <div
+                            class="flex flex-col flex-auto w-full overflow-y-auto antialiased text-gray-800 bg-gray-300 rounded-t-lg h-96">
                             <div id="chat-body" class="flex flex-col flex-auto flex-shrink-0 p-2 my-4">
-    
+
                                 @livewire('chatbox')
-    
+
                             </div>
                         </div>
-    
+
                         @livewire('sendmessage')
                     </div>
                 </div>
